31ca342d7a1f3a34f78315a344bdb20ac9c78e99
sanya1 <okay123456@yandex.ru>
2016-02-18 19:37:06 +0300

    implemented solution and added tests

--- src/main/java/ru/spbau/mit/SecondPartTasks.java
+++ src/main/java/ru/spbau/mit/SecondPartTasks.java
@@ -1,33 +1,90 @@
 package ru.spbau.mit;
 
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public final class SecondPartTasks {
 
-    private SecondPartTasks() {}
+    private SecondPartTasks() {
+    }
 
     // Найти строки из переданных файлов, в которых встречается указанная подстрока.
     public static List<String> findQuotes(List<String> paths, CharSequence sequence) {
-        throw new UnsupportedOperationException();
+        return paths.stream().filter(x -> {
+            try {
+                return Files
+                        .lines(Paths.get(x))
+                        .anyMatch(y -> y.contains(sequence));
+            } catch (IOException e) {
+                return false;
+            }
+        }).collect(Collectors.toList());
     }
 
     // В квадрат с длиной стороны 1 вписана мишень.
     // Стрелок атакует мишень и каждый раз попадает в произвольную точку квадрата.
-    // Надо промоделировать этот процесс с помощью класса java.util.Random и посчитать, какова вероятность попасть в мишень.
+    // Надо промоделировать этот процесс с помощью класса java.util.Random и посчитать,
+    // какова вероятность попасть в мишень.
+    private static final int NTRYES = 1000000;
+    private static final double RADIUS = 0.5;
+    private static final int ADDING_SIZE = 4;
+    private static final int COUNTING_SIZE = 5;
+
     public static double piDividedBy4() {
-        throw new UnsupportedOperationException();
+        return new Random()
+                .doubles(0, 1)
+                .boxed().limit(NTRYES)
+                .map(x -> new ArrayList<Double>(Arrays.asList(0.0, 0.0, 0.0, x)))
+                .reduce(
+                        new ArrayList<Double>(Arrays.asList(0.0, 0.0, 0.0)),
+                        (x, y) -> {
+                            if (y.size() == ADDING_SIZE) {
+                                x.add(y.get(ADDING_SIZE - 1));
+                            }
+                            if (x.size() == COUNTING_SIZE) {
+                                x.set(1, x.get(1) + 1);
+                                double a = x.get(COUNTING_SIZE - 2) - RADIUS;
+                                double b = x.get(COUNTING_SIZE - 1) - RADIUS;
+                                if (a * a + b * b <= RADIUS * RADIUS) {
+                                    x.set(0, x.get(0) + 1);
+                                }
+                                x.remove(COUNTING_SIZE - 1);
+                                x.remove(COUNTING_SIZE - 2);
+                            }
+                            x.set(0, x.get(0) + y.get(0));
+                            x.set(1, x.get(1) + y.get(1));
+                            if (x.get(1) != 0) {
+                                x.set(2, x.get(0) / x.get(1));
+                            }
+                            return x;
+                        }
+                ).get(2);
     }
+
 
     // Дано отображение из имени автора в список с содержанием его произведений.
     // Надо вычислить, чья общая длина произведений наибольшая.
     public static String findPrinter(Map<String, List<String>> compositions) {
-        throw new UnsupportedOperationException();
+        return compositions.entrySet().stream()
+                .max(Comparator
+                        .comparing(x -> x.getValue().stream()
+                                .map(y -> y.length())
+                                .reduce(0, (a, b) -> a + b)))
+                .get().getKey();
     }
 
-    // Вы крупный поставщик продуктов. Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
+    // Вы крупный поставщик продуктов.
+    // Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
     // Необходимо вычислить, какой товар и в каком количестве надо поставить.
     public static Map<String, Integer> calculateGlobalOrder(List<Map<String, Integer>> orders) {
-        throw new UnsupportedOperationException();
+        return orders
+                .stream()
+                .flatMap(x -> x.entrySet().stream())
+                .collect(Collectors
+                        .groupingBy(x -> x.getKey(), Collectors
+                                .summingInt(x -> x.getValue())));
     }
 }
--- src/test/java/ru/spbau/mit/SecondPartTasksTest.java
+++ src/test/java/ru/spbau/mit/SecondPartTasksTest.java
@@ -1,28 +1,131 @@
 package ru.spbau.mit;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static junitx.framework.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class SecondPartTasksTest {
+    private static final String RES_DIR = "res/test";
+
+    private static final List<String> getFiles() {
+        try {
+            return Files
+                    .walk(Paths.get(RES_DIR))
+                    .map(x -> x.toString())
+                    .filter(x -> !x.equals(RES_DIR))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            return Collections.emptyList();
+        }
+    }
+
+    private static final List<String> cutAllFilesSorted(List<String> files) {
+        return files.stream()
+                .map(x -> Paths.get(x).getFileName().toString())
+                .sorted()
+                .collect(Collectors.toList());
+    }
 
     @Test
     public void testFindQuotes() {
-        fail();
+        final List<String> files = getFiles();
+        assertEquals(Arrays.asList("Hello_world"),
+                cutAllFilesSorted(SecondPartTasks.findQuotes(files, "random")));
+        assertEquals(Arrays.asList("Day that never comes"),
+                cutAllFilesSorted(SecondPartTasks.findQuotes(files, "yeaahh")));
+        assertEquals(Arrays.asList("Day that never comes", "Hello_world"),
+                cutAllFilesSorted(SecondPartTasks.findQuotes(files, "word")));
     }
+
+    private static final double EPS = 0.01;
 
     @Test
     public void testPiDividedBy4() {
-        fail();
+        assert (Math.abs(Math.PI / 4 - SecondPartTasks.piDividedBy4()) < EPS);
     }
+
+    private static final List<String> PUSHKIN = Arrays.asList("aaaaaaaaaaaaaaaaaa", "bbbbbbbbbbbbbbbb", "cccccc");
+    private static final List<String> LERMONTOV = Arrays.asList("a", "b", "c");
+    private static final List<String> TWO_PAC = Arrays.asList("f*ck", "yeah",
+            "biiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii***");
+
+    private static final Map<String, List<String>> MAP_AUTHOR_RUSSIAN = new HashMap<String, List<String>>() {
+        {
+            put("PUSHKIN", PUSHKIN);
+            put("LERMONTOV", LERMONTOV);
+        }
+    };
+
+    private static final Map<String, List<String>> MAP_AUTHOR_ALL = new HashMap<String, List<String>>() {
+        {
+            put("PUSHKIN", PUSHKIN);
+            put("LERMONTOV", LERMONTOV);
+            put("2Pac", TWO_PAC);
+        }
+    };
 
     @Test
     public void testFindPrinter() {
-        fail();
+        assertEquals("PUSHKIN", SecondPartTasks.findPrinter(MAP_AUTHOR_RUSSIAN));
+        assertEquals("2Pac", SecondPartTasks.findPrinter(MAP_AUTHOR_ALL));
     }
+
+    private static final Map<String, Integer> PYTEROCHKA = new HashMap<String, Integer>() {
+        {
+            put("SYR", 100);
+            put("MYASO", 100500);
+            put("VODKO", ((int) 3L));
+        }
+    };
+
+    private static final Map<String, Integer> MAGNIT = new HashMap<String, Integer>() {
+        {
+            put("SYR", 5);
+            put("MYASO", 100500);
+            put("VODKO", ((int) 10L));
+            put("VINE PREMIUM", ((int) 1L));
+        }
+    };
+
+    private static final Map<String, Integer> PRODUCTY_PLUS_MINUS = new HashMap<String, Integer>() {
+        {
+            put("OTSTOY", 100500);
+        }
+    };
+
+    private static final List<Map<String, Integer>> PREMIUM_LIST = Arrays.asList(PYTEROCHKA, MAGNIT);
+    private static final List<Map<String, Integer>> CHEAP_LIST = Arrays.asList(PYTEROCHKA, MAGNIT, PRODUCTY_PLUS_MINUS);
+
 
     @Test
     public void testCalculateGlobalOrder() {
-        fail();
+        Assert.assertEquals(new HashMap<String, Integer>() {
+            {
+                put("OTSTOY", 100500);
+                put("VINE PREMIUM", ((int) 1L));
+                put("VODKO", ((int) 13L));
+                put("MYASO", 201000);
+                put("SYR", 105);
+            }
+        }, SecondPartTasks.calculateGlobalOrder(CHEAP_LIST));
+
+        Assert.assertEquals(new HashMap<String, Integer>() {
+            {
+                put("VINE PREMIUM", ((int) 1L));
+                put("VODKO", ((int) 13L));
+                put("MYASO", 201000);
+                put("SYR", 105);
+            }
+        }, SecondPartTasks.calculateGlobalOrder(PREMIUM_LIST));
     }
+
+
 }
