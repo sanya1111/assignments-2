24edb23b5baee52c3f4457cac04ae614f17f2c53
sanya1 <okay123456@yandex.ru>
2016-02-28 15:25:18 +0300

    tests changed

--- src/test/java/ru/spbau/mit/TestLazyCommonUtils.java
+++ src/test/java/ru/spbau/mit/TestLazyCommonUtils.java
@@ -12,61 +12,47 @@ import java.util.concurrent.Future;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-import static junit.framework.Assert.assertEquals;
 import static junit.framework.TestCase.fail;
 
 public final class TestLazyCommonUtils {
-    private TestLazyCommonUtils(){
+    private TestLazyCommonUtils() {
     }
 
-    private static final class LazyChecker<T> implements Supplier<T> {
-        private boolean valid = false;
-
-        @Override
-        public T get() {
-            if (!valid) {
-                fail();
-            }
-            valid = false;
-            return null;
-        }
-
-        public void setValid() {
-            valid = true;
-        }
+    public enum LazyCheckerMode {
+        ONE_ITERATION,
+        SEVERAL_ITERATIONS,
+        SEVERAL_ITERATIONS_DIFF
     }
 
     private static final class LazySameChecker implements Supplier<Object> {
         private int iteration = 0;
-        private int testMode;
+        private LazyCheckerMode testMode;
         private Object retValue;
+        private boolean valid = false;
 
         private static final int MODULO = 10;
 
-        private LazySameChecker(int testMode, Object retValue) {
+        private LazySameChecker(LazyCheckerMode testMode, Object retValue) {
             this.testMode = testMode;
             this.retValue = retValue;
         }
 
         @Override
         public synchronized Object get() {
-            if (testMode == 0 && iteration > 0) {
+            if (!valid || (testMode == LazyCheckerMode.ONE_ITERATION && iteration > 0)) {
                 fail();
             }
             iteration++;
-            if (testMode <= 1 || iteration % MODULO == 0) {
+            if (testMode == LazyCheckerMode.ONE_ITERATION || testMode == LazyCheckerMode.SEVERAL_ITERATIONS
+                    || iteration % MODULO == 0) {
                 return retValue;
             }
             return null;
         }
-    }
 
-    public static <T> void checkLazy(
-                Function<Supplier<T>, Lazy<T>> lazyMaker) {
-        TestLazyCommonUtils.LazyChecker<T> checker = new TestLazyCommonUtils.LazyChecker<>();
-        Lazy<T> lazy = lazyMaker.apply(checker);
-        checker.setValid();
-        assertEquals(lazy.get(), null);
+        public void setValid() {
+            valid = true;
+        }
     }
 
     private static final int NRVALUE = 10000;
@@ -88,13 +74,15 @@ public final class TestLazyCommonUtils {
     }
 
     public static void check(
-            Function<Supplier<Object>, Lazy<Object>> lazyMaker, int threadsNum, int testMode, Object value) {
+            Function<Supplier<Object>, Lazy<Object>> lazyMaker, int threadsNum,
+            LazyCheckerMode testMode, Object value) {
         ExecutorService service = Executors.newFixedThreadPool(threadsNum);
         LazySameChecker checker = new LazySameChecker(testMode, value);
         final Lazy<Object> lazy = lazyMaker.apply(checker);
+        checker.setValid();
         Set<Object> set = putTasksToExecutorServiceAndCalculate(lazy, service);
         Assert.assertEquals(set.size(), 1);
-        if (testMode < 2) {
+        if (testMode == LazyCheckerMode.ONE_ITERATION || testMode == LazyCheckerMode.SEVERAL_ITERATIONS) {
             assert (value == set.iterator().next());
         }
     }
--- src/test/java/ru/spbau/mit/TestLazyFactory.java
+++ src/test/java/ru/spbau/mit/TestLazyFactory.java
@@ -7,55 +7,43 @@ import java.util.function.Supplier;
 
 public class TestLazyFactory {
 
-    public void testSimple(Function<Supplier<Object>, Lazy<Object>> factory, int threadsNum, int testMode) {
+    public void doTestSimple(Function<Supplier<Object>, Lazy<Object>> factory, int threadsNum,
+                             TestLazyCommonUtils.LazyCheckerMode testMode) {
         TestLazyCommonUtils.check(factory, threadsNum, testMode, new Object());
     }
 
-    public void testNull(Function<Supplier<Object>, Lazy<Object>> factory, int threadsNum, int testMode) {
+    public void doTestNull(Function<Supplier<Object>, Lazy<Object>> factory, int threadsNum,
+                           TestLazyCommonUtils.LazyCheckerMode testMode) {
         TestLazyCommonUtils.check(LazyFactory::createLazySingleThread, threadsNum, testMode, null);
     }
 
-    public void testLazy(Function<Supplier<Object>, Lazy<Object>> factory) {
-        TestLazyCommonUtils.checkLazy(factory);
-    }
-
-    public void singleThreadTest(Function<Supplier<Object>, Lazy<Object>> factory) {
-        final int thread = 1;
-        final int testMode = 0;
-
-        testSimple(factory, thread, testMode);
-        testNull(factory, thread, testMode);
-        testLazy(factory);
+    public void runBasicTests(Function<Supplier<Object>, Lazy<Object>> factory, int threads,
+                              TestLazyCommonUtils.LazyCheckerMode testMode) {
+        doTestSimple(factory, threads, testMode);
+        doTestNull(factory, threads, testMode);
     }
 
     private static final int MULTITHREAD_TEST_THREADS = 100;
 
-    public void multiThreadTest(Function<Supplier<Object>, Lazy<Object>> factory, int testMode) {
-
-        testSimple(factory, MULTITHREAD_TEST_THREADS, testMode);
-        testNull(factory, MULTITHREAD_TEST_THREADS, testMode);
-        testLazy(factory);
-    }
 
     @Test
     public void testLazySingleThread() {
-        singleThreadTest(LazyFactory::createLazySingleThread);
+        runBasicTests(LazyFactory::createLazySingleThread, 1,
+                TestLazyCommonUtils.LazyCheckerMode.ONE_ITERATION);
     }
 
     @Test
     public void testLazyMultiThreadSync() {
-        final int testMode = 0;
-
-        multiThreadTest(LazyFactory::createLazyMultiThreadSync, testMode);
+        runBasicTests(LazyFactory::createLazyMultiThreadSync, MULTITHREAD_TEST_THREADS,
+                TestLazyCommonUtils.LazyCheckerMode.ONE_ITERATION);
     }
 
     @Test
     public void testLazyMultiThreadWaitFree() {
-        final int testModeCommon = 1;
-        final int testModeDiff = 2;
-
-        multiThreadTest(LazyFactory::createLazyMultiThreadWaitFree, testModeCommon);
+        runBasicTests(LazyFactory::createLazyMultiThreadWaitFree, MULTITHREAD_TEST_THREADS,
+                TestLazyCommonUtils.LazyCheckerMode.SEVERAL_ITERATIONS);
         TestLazyCommonUtils.check(LazyFactory::createLazyMultiThreadWaitFree,
-                MULTITHREAD_TEST_THREADS, testModeDiff, new Object());
+                MULTITHREAD_TEST_THREADS,
+                TestLazyCommonUtils.LazyCheckerMode.SEVERAL_ITERATIONS_DIFF, new Object());
     }
 }
